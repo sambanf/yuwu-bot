7d7151a69ca818a013df4db4460fc40666341f1f
PERTAMINA\mk.indra.suryajaya <sambanugraha@gmail.com>
2021-09-13 03:21:28 +0700

    Fix Intent

--- mybot.js
+++ mybot.js
@@ -1,5 +1,6 @@
-﻿﻿const Discord = require("discord.js");
-const client = new Discord.Client();
+﻿﻿const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on("ready", () => {
   console.log("I am ready!");
